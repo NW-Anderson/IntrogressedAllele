9c7f0e1231d3316ccfe3eac1376dca4a329ffb10
johnathanlo <49450170+johnathanlo@users.noreply.github.com>
2020-05-14 00:06:40 -0500

    edits

--- R/GetPDF.R
+++ R/GetPDF.R
@@ -7,8 +7,24 @@ GetPDF <- function(n, pop, par){#n = generation number
 
 dvarPhi_I<-function(n, par){##par[1] = d_I, par[2] = phi_I, par[3] = theta_I 
   if(n==0){
-    return((1-par[1])*(par[2]+(par[3]*n+1)*(1-par[2])*exp(par[3]*(n-1))))
+    return((1-par[1])*(par[2]+(par[3]+1)*(1-par[2])*exp(par[3]*(0))))
+  }else{
+    return((1-par[1])*dvarPhi_I(n-1, par)*(par[2]+par[3]*varPhi_I(n-1, par)+1)*(1-par[2])*exp(par[3]*varPhi_I(n-1, par)-1))
   }
-  return((1-par[1])*dvarPhi_I(n-1, par)*(par[2]+par[3]*varPhi_I(n-1, par)+1)*(1-par[2])*exp(par[3]*varPhi_I(n-1, par)-1))
 }
   
+varPhi_I<- function(n, par){
+  if(n==0){
+    return(par[1] + (1-par[1])*(par[2]+(1-par[2]*exp(par[3]*0)))) 
+  }else{
+    return(par[1] + (1-par[1])*(varPhi_I(n-1, par))*(par[2] + (1-par[2])*exp(par[3]*(varPhi_I(n-1, par)-1))))
+  }
+}
+
+dvarPhi_N<-function(n, par){##par[4] = d_N, par[5] = phi_N, par[6] = theta_N, par[10] = h
+  if(n==0){
+    return((1-par[4])*(par[5]+(1-par[5])*exp(par[6])*(1+(exp(par[6]*par[10]*varPhi_I(n-1, par))-1)*(exp(par[6]*(1-par[10])*varPhi_N(n-1, par)-1)))+
+                         varPhi_N(n-1, par)*((1-par[5])*exp(-par[6])*(par[6]*par[10]*dvarPhi_I(n-1, par)*exp(par[6]*par[10]*varPhi_N(n-1, par))*(exp(par[6]*(1-par[10])*varPhi_N(n-1,par))-1))
+                                             +(exp(par[6]*par[10]*varPhi_N(n-1,par))-1)*par[6]*(1-par[10])*dvarPhi_N(n-1,par)*exp(par[6]*(1-par[10])*varPhi_N(n-1,par)))))
+  }
+}
